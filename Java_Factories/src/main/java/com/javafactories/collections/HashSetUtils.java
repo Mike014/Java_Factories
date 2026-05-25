d44382c585d34766153e94930b9d21649713eea7
Michele Grimaldi <mikgrimaldi7@gmail.com>
2026-05-25 15:29:50 +0200

    Refactor HashSetUtils methods to static

--- Java_Factories/src/main/java/com/javafactories/collections/HashSetUtils.java
+++ Java_Factories/src/main/java/com/javafactories/collections/HashSetUtils.java
@@ -5,36 +5,38 @@ import java.util.*;
 public class HashSetUtils {
     private final Scanner myObj = new Scanner(System.in);
 
-    public boolean checkElement(Set<Integer> set, int num) {
+    private HashSetUtils() {}
+
+    public static boolean checkElement(Set<Integer> set, int num) {
         return set.contains(num);
     }
 
-    public void printSetElements(Set<Integer> set) {
+    public static void printSetElements(Set<Integer> set) {
         for (int n : set)
             System.out.println(n);
     }
 
-    public List<Integer> convertSetToList(Set<Integer> set) {
+    public static List<Integer> convertSetToList(Set<Integer> set) {
         return new ArrayList<Integer>(set);
     }
 
-    public TreeSet<Integer> sortSet(Set<Integer> set)
+    public static TreeSet<Integer> sortSet(Set<Integer> set)
     {
         return new TreeSet<>(set);
     }
 
-    public void removeElements(Set<Integer> set, int num) {
+    public static void removeElements(Set<Integer> set, int num) {
         set.removeIf(n -> n < num);
     }
 
-    public Set<Integer> unionSet(Set<Integer> A, Set<Integer>B)
+    public static Set<Integer> unionSet(Set<Integer> A, Set<Integer>B)
     {
         Set<Integer> result = new HashSet<>(A);
         result.addAll(B);
         return result;
     }
 
-    public Set<Integer> diffSet(Set<Integer> A, Set<Integer>B)
+    public static Set<Integer> diffSet(Set<Integer> A, Set<Integer>B)
     {
         Set<Integer> result = new HashSet<>(A);
         result.removeAll(B);
